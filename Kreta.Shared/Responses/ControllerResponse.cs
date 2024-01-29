13a4ddde8e6f260af3632e9754a479405e92e206
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-01-29 15:13:08 +0100

    ErrorStore.cs és ControllerResponse.cs állományok

---
+++ Kreta.Shared/Responses/ControllerResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Kreta.Shared.Responses
+{
+    public class ControllerResponse : ErrorStore
+    {
+        public bool IsSuccess => !HasError;
+
+        public ControllerResponse() : base() { }
+    }
+}
